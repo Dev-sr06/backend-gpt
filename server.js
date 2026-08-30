6a35bac91a1d9238af67359c075c5e120e73ca0b
sumanyu <sumanyuraj7050@gmail.com>
2026-08-30 22:59:17 +0530

    new_commit

--- server.js
+++ server.js
@@ -30,34 +30,34 @@ const connectDB = async() => {
 }
 
 
-// app.post("/test", async (req, res) => {
-//     const options = {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-//         },
-//         body: JSON.stringify({
-//             model: "gpt-4o-mini",
-//             messages: [{
-//                 role: "user",
-//                 content: req.body.message
-//             }]
-//         })
-//     };
+app.post("/test", async (req, res) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-4o-mini",
+            messages: [{
+                role: "user",
+                content: req.body.message
+            }]
+        })
+    };
 
-//     try {
-//         const response = await fetch("https://api.openai.com/v1/chat/completions", options);
-//         const data = await response.json();
-//         //console.log(data.choices[0].message.content); //reply
-//         res.send(data.choices[0].message.content);
-//     } catch(err) {
-//         console.log(err);
-//     }
-// });
+    try {
+        const response = await fetch("https://api.openai.com/v1/chat/completions", options);
+        const data = await response.json();
+        //console.log(data.choices[0].message.content); //reply
+        res.send(data.choices[0].message.content);
+    } catch(err) {
+        console.log(err);
+    }
+});
 
 app.get("/test",(req,res)=>{
     res.json({
-        msg:"test successfully :updated test"
+        msg:"test successfully :updated test again"
     })
 })
